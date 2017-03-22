f01c4abfc111059fb8d85d0132a3c8d5886d74e8
Thomas Jespersen <tje@mentum.dk>
2017-03-22 15:27:28 +0100

    Enable start/stop alarm from command line

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
+var wpi = null;
 try {
-	var wpi = require('wiring-pi');
+	wpi = require('wiring-pi');
 
 	// GPIO pin of the led
 	var configPin = 7;
@@ -22,3 +23,22 @@ try {
 	console.log("Failed to run WPI");
 	wpi = null
 }
+
+var changeAlarmState = function(state){
+	var newAlarmState = state === "start" ? 1 : 0;
+	if(wpi === null) {
+		console.log("Alarm " + newAlarmState);
+	} else {
+		wpi.digitalWrite(configPin, newAlarmState);
+	} 
+}
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on('line', (line) => {
+	changeAlarmState(line);
+});
